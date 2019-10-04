158c7ce7c7c313b2e7b077580e57e76d0f48cde8
tsen1220 <tsen1220.us@gmail.com>
2019-10-05 00:22:29 +0800

    Firstcommit

---
+++ backend/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tsen1\desktop\django~1\backend\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ frontend/gui/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { BrowserRouter as Router } from "react-router-dom";
+
+import BaseRouter from "./routes";
+
+import CustomLayout from "./containers/Layout";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <CustomLayout>
+          <BaseRouter />
+        </CustomLayout>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
