f0dd3706da30205bb1e26928c0588d49068e6b8e
tsen1220 <tsen1220.us@gmail.com>
2019-10-06 14:08:40 +0800

    loginSet

--- backend/src/djreact/urls.py
+++ backend/src/djreact/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_framework.urls')),
     path('api/', include('articles.api.urls'))
 ]
--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,21 +1,42 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import { BrowserRouter as Router } from "react-router-dom";
-
+import { connect } from "react-redux";
 import BaseRouter from "./routes";
+import * as actions from "./store/actions/auth";
 
 import CustomLayout from "./containers/Layout";
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <CustomLayout>
-          <BaseRouter />
-        </CustomLayout>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <CustomLayout {...this.props}>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.token !== null
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
