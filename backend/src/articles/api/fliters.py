09d77a42296642f2ee7a4c8ad30dc8397cd0ce65
tsen1220 <tsen1220.us@gmail.com>
2019-10-09 12:58:38 +0800

    filter

--- backend/src/articles/api/fliters.py
+++ backend/src/articles/api/fliters.py
@@ -5,4 +5,4 @@ from articles.models import Comment
 class CommentFilter(django_filters.FilterSet):
     class Meta:
         model = Comment
-        fields = ['article']
+        fields = ['article', 'user']
